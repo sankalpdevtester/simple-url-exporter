3decb186bf5ed95c3ff6489ac4c3e75814c05c47
sankalpdevtester <ohofufic0@gmail.com>
2026-08-03 14:43:39 +0530

    chore: add config

---
+++ config/app.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'name' => env('APP_NAME', 'Simple URL Exporter'),
+    'env' => env('APP_ENV', 'production'),
+    'debug' => env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'asset_url' => env('ASSET_URL', null),
+    'timezone' => 'UTC',
+    'locale' => 'en',
+    'fallback_locale' => 'en',
+    'faker_locale' => 'en_US',
+    'key' => env('APP_KEY'),
+    'cipher' => 'AES-256-CBC',
+    'providers' => [
+        // ...
+    ],
+    'aliases' => [
+        // ...
+    ],
+];
